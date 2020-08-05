5f3d02cf0bf11b46ea71220584455cbb81875fb2
ing-arriola <ingeniero.arriola@gmail.com>
2020-08-05 15:43:43 -0600

    Added some logic to deal with infinity scroll problem.. hopefully soon I will get that done

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
-
+let ready = false;
+let images = 0;
+let totalImages = 0;
 let phothosArray = [];
 
 const count = 15;
-const apiKey = "";
+const apiKey = "GhS2Wfkv4IveYrDdCoW-OEXb4ffFsHpVRGTZScbO9PY";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 const setAttributes = (element, attributes) => {
@@ -13,7 +15,18 @@ const setAttributes = (element, attributes) => {
   }
 };
 
+const imagesLoaded = () => {
+  images++;
+  if (images === totalImages) {
+    ready = true;
+    console.log(images);
+  } else {
+  }
+};
+
 const displayPhotos = () => {
+  totalImages = phothosArray.length;
+  console.log(totalImages);
   phothosArray.forEach((photo) => {
     const item = document.createElement("a");
     setAttributes(item, {
@@ -21,14 +34,12 @@ const displayPhotos = () => {
       target: "_blank",
     });
     const img = document.createElement("img");
-    //img.setAttribute("src", photo.urls.regular);
-    //img.setAttribute("alt", photo.alt_description);
-    //img.setAttribute("title", photo.alt_description);
     setAttributes(img, {
       src: photo.urls.regular,
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+    img.addEventListener("load", imagesLoaded);
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
@@ -43,5 +54,15 @@ const getPhotos = async () => {
     console.log(err);
   }
 };
+
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >=
+    document.body.offsetHeight - 1000
+  ) {
+    console.log("load more");
+    getPhotos();
+  }
+});
 
 getPhotos();
