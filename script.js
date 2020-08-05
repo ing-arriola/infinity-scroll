d11cbc2b60c45c171ad4f4daffdaffe9160f57d4
ing-arriola <ingeniero.arriola@gmail.com>
2020-08-04 19:46:09 -0600

    Implemented  DRY principle to get rid of some nasty setAttributes lines that doesn't look good

--- script.js
+++ script.js
@@ -7,15 +7,28 @@ const count = 15;
 const apiKey = "";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+const setAttributes = (element, attributes) => {
+  for (const att in attributes) {
+    element.setAttribute(att, attributes[att]);
+  }
+};
+
 const displayPhotos = () => {
   phothosArray.forEach((photo) => {
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "_blank");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    //img.setAttribute("src", photo.urls.regular);
+    //img.setAttribute("alt", photo.alt_description);
+    //img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
